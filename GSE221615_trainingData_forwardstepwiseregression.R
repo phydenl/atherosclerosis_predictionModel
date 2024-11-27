b856ce63dcf2e7934262ae39c4ea30c6d01e4ce2
lo_phy <147338059+phydenl@users.noreply.github.com>
2024-11-27 16:18:16 -0500

    Add files via upload

--- GSE221615_trainingData_forwardstepwiseregression.R
+++ GSE221615_trainingData_forwardstepwiseregression.R
@@ -3,11 +3,12 @@ library(ggplot2)
 library(dplyr)
 library(glmnet)
 
-sex_likelihoodratiotest <- lrtest(fit_withsex_relaxed,fit_withoutsex_relaxed )
-###no relaxed or glmnet objects can be used with lrtest 
+setwd("E:/Atherosclerosis_predictionModel_2024")
+total_trainingdata_geneExpression <- read.csv("E:/Atherosclerosis_predictionModel_2024/GSE221615_trainingdata_geneExpression_w_clinicalFactors.csv", row.names = 1)
+total_trainingdata_meta <- read.csv("E:/Atherosclerosis_predictionModel_2024/GSE221615_trainingdata_metadata.csv")
 
-null_test <- lm(total_trainingdata_geneExpression~1, data = total_trainingdata_geneExpression)
-lm(pesa_score_responseVariable~1, data = total_trainingdata_geneExpression)
+# null_test <- lm(total_trainingdata_geneExpression~1, data = total_trainingdata_geneExpression)
+# lm(pesa_score_responseVariable~1, data = total_trainingdata_geneExpression)
 
 
 
@@ -16,13 +17,13 @@ lm(pesa_score_responseVariable~1, data = total_trainingdata_geneExpression)
 ##the empty model can be either a numeric or character vector
 ## data must contain BOTH dependent (pesa score) AND independent (gene ID, sex, age) 
 
-
+pesa_score_responseVariable <- total_trainingdata_meta$pesa_score
 
 total_trainingdata_geneExpression$pesa_score <- pesa_score_responseVariable
 total_trainingdata_geneExpression <- total_trainingdata_geneExpression %>% select(pesa_score, everything())
-total_trainingdata_geneExpression$age <- total_trainingdata$Age
 
-
+##### not working -- stepwise regression#######
 empty_forward_model <- lm(pesa_score ~ 1, data = total_trainingdata_geneExpression) 
 all(is.na(total_trainingdata_geneExpression$pesa_score))
 
+all(is.nan(total_trainingdata_geneExpression$pesa_score))
